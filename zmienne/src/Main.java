dd8bce1d3fed30184352a8b4a8c67e621b533561
biku89 <biklukasz@gmail.com>
2025-02-23 15:39:05 +0100

    Merge pull request #8 from biku89/nowyBranch1
    Merge branch 'main' into nowyBranch2

--- zmienne/src/Main.java
+++ zmienne/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("NajjnowszyTestBranch");
+        System.out.println("SprawdzamJakDziałaBranch");
     }
 }
