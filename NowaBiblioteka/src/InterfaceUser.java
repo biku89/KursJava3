6670cbfba494af52ad56bb4776f5b8700bd60ef9
biku89 <biklukasz@gmail.com>
2025-03-22 17:48:32 +0100

    Dodaje aktualizację plików

--- NowaBiblioteka/src/InterfaceUser.java
+++ NowaBiblioteka/src/InterfaceUser.java
@@ -1,7 +1,61 @@
 import java.util.Scanner;
 
 public class InterfaceUser {
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner;
     Library library;
 
+    public InterfaceUser(Scanner scanner, Library library) {
+        this.scanner = scanner;
+        this.library = library;
+        this.library = new Library();
+
+    }
+
+    public void showMenu(){
+        while (true){
+            displayMenu();
+
+            try {
+                int choice = scanner.nextInt();
+                this.scanner.nextLine();
+                switch (choice){
+                    case 0 ->{
+                        System.out.println("Kończymy działanie programu");
+                        return;
+                    }
+                    case 1 ->{
+                        System.out.println("Dostępna lista: ");
+                        library.printAllTitle();
+                    }
+                    case 2 ->{
+                        System.out.println("Jaki tytuł chcesz wypożyczyć ? ");
+                        String title = scanner.nextLine();
+                        this.library.rentItem(title);
+                    }
+                    case 3 ->{
+                        System.out.println("Podaj tytuł do zwortu");
+                        String title = scanner.nextLine();
+                        library.returnItem(title);
+                    }
+                    case 4 -> {
+                        System.out.println("Liczba książek: " + Book.getCountBook());
+                        System.out.println("Liczba Filmów: " + Film.getCountFilm());
+                    }
+                    case 5 ->{
+                        System.out.println("Dostpęna lista tytułów do wypożyczenia");
+                    }
+                }
+            }catch (ItemAlredyReturnedException e){
+                System.err.println(":(");
+            }
+        }
+    }
+
+    private void displayMenu() {
+        System.out.println("Wybierz jedną z opcji");
+        System.out.println("1 - Wyświetl zawartość biblioteki");
+        System.out.println("2 - Wypożyczenie");
+        System.out.println("3 - Zwrot");
+        System.out.println("4 - Liczba książek i filmów");
+    }
 }
