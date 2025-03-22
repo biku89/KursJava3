69fb36307614f2cbaa21f182353b67fb05aef93e
biku89 <biklukasz@gmail.com>
2025-03-22 15:23:21 +0100

    Dodaje podstawowe pliki dla testu

---
+++ NowaBiblioteka/src/Book.java
@@ -0,0 +1,11 @@
+public class Book extends LibraryItem {
+    private String author;
+    private int page;
+
+    public Book(String author, int page) {
+        this.author = author;
+        this.page = page;
+    }
+
+
+}
---
+++ NowaBiblioteka/src/Film.java
@@ -0,0 +1,11 @@
+public class Film extends LibraryItem{
+    private String directory;
+    private double durationFilm;
+
+    public Film(String directory, double durationFilm) {
+        this.directory = directory;
+        this.durationFilm = durationFilm;
+    }
+
+
+}
